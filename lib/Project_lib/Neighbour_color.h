a913e41665578d73a5e3e77f570c6abe827d2d33
Francis <francischan@kingsphase.page>
2023-02-23 12:30:50 +0800

    New file indicate color ciode

---
+++ lib/Project_lib/Neighbour_color.h
@@ -0,0 +1,8 @@
+#ifndef Neighbout_color_h
+#define Neighbout_color_h
+
+#define TFT_NEIGHBOUR_BEIGE     0xEF1A
+#define TFT_NEIGHBOUR_GREEN     0x12E8
+#define TFT_NEIGHBOUR_BLUE      0x5EBC
+
+#endif
