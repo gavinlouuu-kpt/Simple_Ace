11771b66cda4f991716fdf673cc87cc5d5fa9289
Francis <francischan@kingsphase.page>
2023-03-08 12:53:53 +0800

    Edit  heater feature test

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <PID.h>
 #include <Cloud_storage.h>
 #include <Wifi_connection.h>
+#include <Adafruit_ADS1X15.h>
+extern Adafruit_ADS1115 ads;
 
 unsigned long millis_previousCheckTime =0;
 
@@ -18,12 +20,39 @@ void setup() {
   HomeScreen();
 }
 
+unsigned long previous_switch_heater =0;
+extern double Output;
+int dac_power=255;
+
 void loop() {
   PID_control();
   Navigation();
-  if(millis()-millis_previousCheckTime > 10000){
-    check_sensor_life();
-    millis_previousCheckTime = millis();
+  Serial.print(ads.readADC_SingleEnded(Sensor_channel));
+  Serial.print(",");
+  Serial.print(ads.readADC_SingleEnded(Heater_channel));
+  Serial.print(",");
+  Serial.print(ads.readADC_SingleEnded(Offset_channel));
+  Serial.print(",");
+  Serial.print(ads.readADC_SingleEnded(NTCC_channel));
+  Serial.print(",");
+  Serial.println(Output); 
+  if(millis()- previous_switch_heater >10000){
+    previous_switch_heater = millis();
+    if(dac_power == 255){
+      dac_power = 0;
+      dacWrite(sensor_h, dac_power);
+      delay(10);
+    }
+    else{
+      dac_power = 255;
+      dacWrite(sensor_h, dac_power);
+      delay(10);
+    } 
   }
+  
+  // if(millis()-millis_previousCheckTime > 10000){
+  //   check_sensor_life();
+  //   millis_previousCheckTime = millis();
+  // }
 }
 
