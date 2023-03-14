f842d8fa01e453268d6f3e8f3ad49f7e91a10651
sa2006026 <sa200602687@yahoo.com>
2023-03-14 16:33:33 +0800

    test

--- lib/Project_lib/Screen.h
+++ lib/Project_lib/Screen.h
@@ -1,16 +1,16 @@
 #ifndef Screen_h
 #define Screen_h
 
-
-#define TFT_NEIGHBOUR_BEIGE     0xEF1A
-#define TFT_NEIGHBOUR_GREEN     0x12E8
-#define TFT_NEIGHBOUR_BLUE      0x5EBC
+#define TFT_NEIGHBOUR_BEIGE 0xEF1A
+#define TFT_NEIGHBOUR_GREEN 0x12E8
+#define TFT_NEIGHBOUR_BLUE 0x5EBC
+// test
 
 void draw_framework();
 void tft_setup();
 void draw_sensor(double value);
 void draw_time(int time);
-void draw_result(double ace,double co2);
+void draw_result(double ace, double co2);
 void draw_wait(void);
 void TouchScreen();
 void draw_Wifi();
@@ -18,4 +18,3 @@ void draw_progress(float bar_time, float bar_percentage);
 void draw_loading(int count);
 void HomeScreen();
 #endif
-
